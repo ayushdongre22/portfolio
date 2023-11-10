08b4b3cb65e7954a5abf7d2eb673be286ae08b4f
Ayush Dongre <87798106+ayushdongre22@users.noreply.github.com>
2023-11-10 18:15:23 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ function About() {
           </div>
         </div>
         <div className="col-md-5">
-          <img id="pictureself" src={Pic} alt="Aryan" />
+          <img id="pictureself" src={Pic} alt="Ayush" />
         </div>
       </div>
     </div>
